3b79681180d6c6e3bea25f51d9ba20eba8770edb
Lcarused <donghao19950925@gmail.com>
2019-01-14 08:58:06 +0800

    threading + queue

--- lib_queue_demo.py
+++ lib_queue_demo.py
@@ -19,7 +19,7 @@ def worker(i):
 
 if __name__ == '__main__':
     # 指定线程数
-    num_of_threads = 5
+    num_of_threads = 40
 
     # 创建一个FIFO队列对象，不指定maxsize
     q = queue.Queue()
@@ -32,7 +32,7 @@ if __name__ == '__main__':
         t.start()
 
     # 模拟20个任务
-    source = [i for i in range(1, 21)]
+    source = [i for i in range(1, 100000000)]
 
     # 将任务源里的任务逐个放入队列，每隔0.5秒发布一个新任务
     for item in source:
